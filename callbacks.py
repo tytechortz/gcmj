f8c9d3828590edabd7b0fa0e36f0d96653372058
James Swank <jmswank7@gmail.com>
2021-04-27 14:20:35 -0600

    rev scatter works

--- callbacks.py
+++ callbacks.py
@@ -2,12 +2,11 @@ import dash_html_components as html
 import dash_core_components as dcc
 import pandas as pd
 import os
-
 from dash.dependencies import Input, Output, State
-
 from app import app
-from data import df_revenue, sources 
+from data import df_revenue, sources, df_rev
 from dotenv import load_dotenv
+import plotly.graph_objs as go
 
 load_dotenv()
 
@@ -16,7 +15,6 @@ app.layout = html.Div([
     html.Div(id='page-content')
 ])
 
-
 @app.callback(
      Output('revenue-map', 'figure'),
      Input('year', 'value'))         
@@ -24,7 +22,6 @@ def update_rev_map(selected_year):
    
     year1 = selected_year
   
-     
     df_year = df_revenue.loc[df_revenue['year'] == selected_year]
     df_smr = pd.DataFrame({'county': df_year['county'], 'year': df_year.year, 'total revenue': df_year.tot_sales,'CENT_LAT':df_year.CENT_LAT,
                     'CENT_LON':df_year.CENT_LONG, 'marker_size':(df_year.tot_sales)*(.35**14)})
@@ -70,3 +67,53 @@ def update_rev_map(selected_year):
             )
     fig = dict(data=data, layout=layout)
     return fig
+
+@app.callback(
+    Output('rev-scatter', 'figure'),
+    [Input('revenue-map', 'clickData'),
+    Input('year','value'),
+    Input('rev', 'value')])
+def create_rev_scat(clickData,year,rev):
+    year_df = df_rev[df_rev['year'] == str(year)]
+
+    filtered_df = year_df[year_df['county'] == clickData['points'][-1]['text']]
+    
+    filtered_df = filtered_df.sort_values('month')
+   
+    labels = ['Feb', 'Apr', 'Jun','Aug','Oct','Dec']
+    tickvals = [2,4,6,8,10,12]
+    traces = []
+
+    if 'TOTAL' in rev:
+            traces.append(go.Scatter(
+            x = filtered_df['month'],
+            y = filtered_df['tot_sales'],
+            name = 'Total Sales',
+            line = {'color':'red'} 
+            ))
+    if 'REC' in rev:  
+            traces.append(go.Scatter(
+            x = filtered_df['month'],
+            y = filtered_df['rec_sales'],
+            name = 'Rec Sales',
+            line = {'color':'dodgerblue'}
+            ))
+    if 'MED' in rev:  
+            traces.append(go.Scatter(
+            x = filtered_df['month'],
+            y = filtered_df['med_sales'],
+            name = 'Med Sales',
+            line = {'color':'black'}
+            ))
+
+    return {
+            'data': traces,
+            'layout': go.Layout(
+                xaxis = {'title': 'Month','tickvals':tickvals,'tickmode': 'array','ticktext': labels},
+                yaxis = {'title': 'Revenue'},
+                hovermode = 'closest',
+                title = '{} COUNTY {} REVENUE - {}'.format(clickData['points'][-1]['text'],rev,year),
+                height = 350,
+                font = {'size': 8}
+            )
+        }
