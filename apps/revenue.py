26550252a3caf8c63a96e9d43d4ac473ec8b7eea
James Swank <jmswank7@gmail.com>
2021-04-27 23:02:13 -0600

    all in box

--- apps/revenue.py
+++ apps/revenue.py
@@ -76,9 +76,86 @@ def revenue_App():
                     ],
                         className='col-4'
                     ),
-            ],
-                className='row'
-            ),
+                ],
+                    className='row'
+                ),
+                get_emptyrow(),
+                html.Div([
+                    html.Div([
+                        dcc.Slider(
+                            id='year',
+                            min=2014,
+                            max=2020,
+                            step=1,
+                            marks={x: '{}'.format(x) for x in range(2014, 2021)},
+                            value=2014
+                        ),
+                    ],
+                        className='col-8'
+                    ),
+                    html.Div([
+                        dcc.RadioItems(
+                            id='month-year', 
+                            options=[
+                                {'label':'Year', 'value':'yr'},
+                                {'label':'Month', 'value':'mo'},
+                            ],
+                            labelStyle={'display':'inline-block', 'margin': 0, 'padding': 1},
+                            value='mo',
+                            style = {'text-align': 'center'}
+                        ),
+                    ],
+                        className='col-4'
+                    ),
+                ],
+                    className='row'
+                ),
+                html.Div([
+                    html.Div([
+                        dcc.Checklist(
+                            id='rev',
+                            options=[
+                                {'label':'Total Sales', 'value':'TOTAL'},
+                                {'label':'Rec Sales','value':'REC'},
+                                {'label':'Med Sales','value':'MED'},
+                            ],
+                            labelStyle={'display':'inline-block', 'margin': 0, 'padding': 1},
+                            value = ['TOTAL'],
+                            style = {'text-align': 'center'}
+                            ),
+                    ],
+                        className='col-6'
+                    ),
+                    html.Div([
+                        dcc.Slider(
+                            id='month',
+                            min=1,
+                            max=12,
+                            step=1,
+                            marks={x: '{}'.format(x) for x in range(1, 13)},
+                            value=1
+                        )
+                    ],
+                        className='col-6'
+                    ),
+                ],
+                    className='row'
+                ),
+                html.Div([
+                    html.Div([
+                        dcc.Graph(id='rev-scatter')
+                    ],
+                        className='col-6'
+                    ),
+                    html.Div([
+                        dcc.Graph(id='month-rev-bar')
+                    ],
+                        className='col-6'
+                    ),
+                ],
+                    className='row'
+                ),
+                get_emptyrow(),
             ],
                 className='col-10',
                 style = externalgraph_colstyling, # External 10-column
@@ -92,226 +169,7 @@ def revenue_App():
         style=externalgraph_rowstyling, # External row
         ),
         get_emptyrow(),
-        html.Div([
-            html.Div([
-            ],
-                className='col-1', # Blank 1 column
-            ),
-            html.Div([
-                dcc.Slider(
-                    id='year',
-                    min=2014,
-                    max=2020,
-                    step=1,
-                    marks={x: '{}'.format(x) for x in range(2014, 2021)},
-                    value=2014
-                ),
-            ],
-                className='col-6'
-            ),
-            html.Div([
-                dcc.RadioItems(
-                    id='month-year', 
-                    options=[
-                        {'label':'Year', 'value':'yr'},
-                        {'label':'Month', 'value':'mo'},
-                    ],
-                    labelStyle={'display':'inline-block', 'margin': 0, 'padding': 1},
-                    value='mo',
-                    style = {'text-align': 'center'}
-                ),
-            ],
-                className='col-4'
-            ),
-            html.Div([
-            ],
-                className='col-1', # Blank 1 column
-            ),
-        ],
-            className = 'row',
-            style = externalgraph_rowstyling, # External row
-        ),
-        get_emptyrow(),
-        html.Div([
-            html.Div([
-            ],
-                className='col-1', # Blank 1 column
-            ),
-            html.Div([
-                dcc.Checklist(
-                    id='rev',
-                    options=[
-                        {'label':'Total Sales', 'value':'TOTAL'},
-                        {'label':'Rec Sales','value':'REC'},
-                        {'label':'Med Sales','value':'MED'},
-                    ],
-                    labelStyle={'display':'inline-block', 'margin': 0, 'padding': 1},
-                    value = ['TOTAL'],
-                    style = {'text-align': 'center'}
-                    ),
-            ],
-                className='col-6'
-            ),
-            html.Div([
-                dcc.Slider(
-                    id='month',
-                    min=1,
-                    max=12,
-                    step=1,
-                    marks={x: '{}'.format(x) for x in range(1, 13)},
-                    value=1
-                )
-            ],
-                className='col-4'
-            ),
-            html.Div([
-            ],
-                className='col-1', # Blank 1 column
-            ),
-        ],
-            className = 'row',
-            style = externalgraph_rowstyling, # External row
-        ),
-        html.Div([
-            html.Div([
-                dcc.Graph(id='rev-scatter')
-            ],
-                className='col-6'
-            ),
-            html.Div([
-                dcc.Graph(id='month-rev-bar')
-            ],
-                className='col-6'
-            ),
-        ],
-            className = 'row',
-            style = externalgraph_rowstyling, # External row
-        ),
         html.Div(id='crat', style={'display': 'none'}),
     ])
-
-# def revenue_App():
-#     return html.Div([
-#         html.Div([
-#             html.Div([
-#                 html.Div([
-#                     dcc.Link('Home', href='/')
-#                 ],
-#                     className='two columns'
-#                 ),
-#                 html.H4('Revenue Data',
-#                 className='twelve columns',
-#                 style={'text-align': 'center'}
-#             ),
-
-#             ],
-#                 className='twelve columns'
-#             ),
-
-#         ],
-#             className='row'
-#         ),
-#         html.Div([
-            
-            
-#         ],
-#             className='row'
-#         ),
-#         html.Div([
-#             html.Div([
-#                 html.Div([
-#                     dcc.Graph('revenue-map')
-#                 ],
-#                     className='twelve columns'
-#                 ),
-#             ],
-#                 className='eight columns'
-#             ),
-#             html.Div([
-                # dcc.Markdown('''Click on counties and use year slider to see annual county revenue data displayed in graphs.  Green counties have at least one form of legalized cannabis, green circles show relative cannabis revenue for selected year. 
-                # Select sales check boxes to display revenue graphically by type below left. Select Year or Month button below to display county revenue by month across years, or to display cumulative revenue totals for each month, respectively.''')
-#             ],
-#                 className='four columns'
-#             ),
-#         ],
-#             className='row'
-#         ),
-#         html.Div([
-#             html.Div([
-                # dcc.Slider(
-                #     id='year',
-                #     min=2014,
-                #     max=2020,
-                #     step=1,
-                #     marks={x: '{}'.format(x) for x in range(2014, 2021)},
-                #     value=2014
-                # ),
-#             ],
-#                 className='eight columns'
-#             ),
-            # html.Div([
-            #     dcc.RadioItems(id='month-year', 
-            #     options=[
-            #         {'label':'Year', 'value':'yr'},
-            #         {'label':'Month', 'value':'mo'},
-            #     ],
-            #     labelStyle={'display':'inline-block', 'margin': 0, 'padding': 1},
-            #     value='mo',
-            #     style = {'text-align': 'center'}
-            #     ),
-#             ],
-#                 className='four columns'
-#             ),
-#         ],
-#             className='row'
-#         ),
-#         html.Div([
-#             html.Div([
-                # dcc.Checklist(
-                #     id='rev',
-                #     options=[
-                #         {'label':'Total Sales', 'value':'TOTAL'},
-                #         {'label':'Rec Sales','value':'REC'},
-                #         {'label':'Med Sales','value':'MED'},
-                #     ],
-                #     labelStyle={'display':'inline-block', 'margin': 0, 'padding': 1},
-                #     value = ['TOTAL'],
-                #     style = {'text-align': 'center'}
-                #     ),
-#             ],
-#                 className='six columns'
-#             ),
-#             html.Div([
-                # dcc.Slider(
-                #     id='month',
-                #     min=1,
-                #     max=12,
-                #     step=1,
-                #     marks={x: '{}'.format(x) for x in range(1, 13)},
-                #     value=1
-                # )
-#             ],
-#                 className='six columns'
-#             ),
-#         ],
-#             className='row'
-#         ),
-#         html.Div([
-            # html.Div([
-            #     dcc.Graph(id='rev-scatter')
-            # ],
-            #     className='six columns'
-            # ),
-#             html.Div([
-#                 dcc.Graph(id='month-rev-bar')
-#             ],
-#                 className='six columns'
-#             ),
-#         ],
-#             className='row'
-#         ),
-        # html.Div(id='crat', style={'display': 'none'}),
-#     ])
-
 
 app.layout = revenue_App
